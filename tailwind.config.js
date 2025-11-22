d601622ff48ec8206c676951cf7be30ec97231b4
thanhhuyvochau <thanhhuyvochau@gmail.com>
2025-11-22 19:59:40 +0700

    chore: add tailwin config for suggestion

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    './app/**/*.{vue,js,ts,jsx,tsx,html}',
+    './components/**/*.{vue,js}',
+    './pages/**/*.{vue,js}',
+    './layouts/**/*.{vue,js}',
+    './app.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
